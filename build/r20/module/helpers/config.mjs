359e1ba09642d3191e318f55e517e11ccb5cea82
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2024-02-22 16:33:18 -0300

    run yarn generate

---
+++ build/r20/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const R20 = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+R20.abilities = {
+  str: 'R20.Ability.Str.long',
+  dex: 'R20.Ability.Dex.long',
+  con: 'R20.Ability.Con.long',
+  int: 'R20.Ability.Int.long',
+  wis: 'R20.Ability.Wis.long',
+  cha: 'R20.Ability.Cha.long',
+};
+
+R20.abilityAbbreviations = {
+  str: 'R20.Ability.Str.abbr',
+  dex: 'R20.Ability.Dex.abbr',
+  con: 'R20.Ability.Con.abbr',
+  int: 'R20.Ability.Int.abbr',
+  wis: 'R20.Ability.Wis.abbr',
+  cha: 'R20.Ability.Cha.abbr',
+};
