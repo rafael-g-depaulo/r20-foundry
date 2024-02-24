26ad213537cac2015992461bc589da8403945632
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2024-02-24 19:35:59 -0300

    temp

---
+++ updateSystemVersion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# IMPORTANT: this needs the semver utility from https://github.com/fsaintjacques/semver-tool
+
+OLD_VERSION=$(cat ./package.json | jq --raw-output '.version')
+CHANGE_TYPE=$(
+	echo "major minor patch" |
+		tr ' ' '\n' |
+		fzf --preview="echo \"$OLD_VERSION -> \$(semver bump {} $OLD_VERSION)\""
+)
+
+NEW_VERSION=$(semver bump $CHANGE_TYPE $OLD_VERSION)
+
+# update system.json
+cat ./system.json |
+	jq ". + { version: \"$NEW_VERSION\", download: \"https://github.com/rafael-g-depaulo/r20-foundry/archive/refs/tags/v$NEW_VERSION.zip\"}" |
+	tee ./system.json &>/dev/null
+
+# update package.json
+cat ./package.json |
+	jq ". + {version: \"$NEW_VERSION\"}" |
+	tee ./package.json &>/dev/null
+
+# add git tag
+git tag v$NEW_VERSION &>/dev/null
+
+echo "changed package semver: $OLD_VERSION -> $NEW_VERSION"
