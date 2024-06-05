7348b85010f1a549811d68f225ec1c72a9d00461
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2024-06-04 23:45:45 -0300

    Add typedefs

---
+++ module/typedefs/CharacterTypedef.mjs
@@ -0,0 +1,36 @@
+
+
+/**
+ * @typedef {Object} Resource
+ * @property {number} value
+ * @property {number} max
+ * @property {number} bonus
+ */
+
+/**
+ * @typedef {Object} Resources
+ * @property {Resource} hp
+ * @property {Resource} mp
+ */
+
+/**
+ * @typedef {Object} Attribute
+ * @property {number} value
+ * @property {number} bonus
+ */
+
+/**
+ * @typedef {Object} Attributes
+ * @property {Attribute} str
+ * @property {Attribute} dex
+ * @property {Attribute} con
+ * @property {Attribute} int
+ * @property {Attribute} sen
+ * @property {Attribute} pre
+ */
+
+/**
+ * @typedef {Object} R20Character
+ * @property {Attributes} attributes
+ * @property {Resources} resources
+ */
---
+++ module/typedefs/PcTypedef.mjs
@@ -0,0 +1,42 @@
+/**
+ * @typedef {import('./CharacterTypedef.mjs').Resource} Resource
+ */
+
+/**
+ * @typedef {Object} Resources
+ * @property {Resource} hp
+ * @property {Resource} mp
+ * @property {Resource} fp
+ */
+
+/**
+ * @typedef {Object} AbilityModifiers
+ * @property {number} STR
+ * @property {number} DEX
+ * @property {number} CON
+ * @property {number} INT
+ * @property {number} SEN
+ * @property {number} PRE
+ */
+
+/** @typedef {Object} CharConfig
+ *
+ * @property {number} lv1MaxHp
+ * @property {number} hpPerLevel
+ * @property {number} mpPerLevel
+ *
+ * @property {number} bonusMaxHP
+ * @property {number} bonusMaxMP
+ * @property {number} bonusMaxFP
+ *
+ * @property {boolean} isPaladin
+ */
+
+/**
+ * @typedef {Object} _R20Pc
+ * @property {number} level
+ * @property {Resources} resources
+ * @property {CharConfig} config
+ *
+ * @typedef {import('./CharacterTypedef.mjs').R20Character & _R20Pc & AbilityModifiers} R20Pc
+ */
